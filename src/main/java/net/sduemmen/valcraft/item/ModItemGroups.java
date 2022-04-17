969107db8da416636ba3c095d40e954fe9e49000
Samuel Dümmen <samuel.duemmen@gmx.de>
2022-04-17 22:06:29 +0200

    refactor

--- src/main/java/net/sduemmen/valcraft/item/ModItemGroups.java
+++ src/main/java/net/sduemmen/valcraft/item/ModItemGroups.java
@@ -4,13 +4,13 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModItemGroups {
-    public static final CreativeModeTab VALCRAFT_TOOLS_ITEM_GROUP = new CreativeModeTab("valcraft") {
+    public static final CreativeModeTab VALCRAFT_TOOLS_ITEM_GROUP = new CreativeModeTab("valcraft_tools_weapons") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModTools.CRYSTAL_BATTLEAXE.get());
         }
     };
-    public static final CreativeModeTab VALCRAFT_MATERIALS_ITEM_GROUP = new CreativeModeTab("valcraft") {
+    public static final CreativeModeTab VALCRAFT_MATERIALS_ITEM_GROUP = new CreativeModeTab("valcraft_materials") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.CRYSTAL.get());
