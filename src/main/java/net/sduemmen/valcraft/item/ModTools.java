791ceca0e137704017eed3fc90621e9940b61fd9
Samuel Dümmen <samuel.duemmen@gmx.de>
2022-05-14 07:24:01 +0200

    changed Crystal_Battleaxe to be of type AxeItem

--- src/main/java/net/sduemmen/valcraft/item/ModTools.java
+++ src/main/java/net/sduemmen/valcraft/item/ModTools.java
@@ -1,7 +1,7 @@
 package net.sduemmen.valcraft.item;
 
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,8 +12,11 @@ public class ModTools {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Valcraft.MOD_ID);
 
     public static final RegistryObject<Item> CRYSTAL_BATTLEAXE = ITEMS.register("crystal_battleaxe",
-            () -> new SwordItem(ModTiers.CRYSTAL_BATTLEAXE, 9, -3.5f, new Item.Properties() // dmg = 1 + pAttackDamageModifier; speed = 4 + pAttackSpeedModifier
+            () -> new AxeItem(ModTiers.CRYSTAL_BATTLEAXE, 9, -3.5f, new Item.Properties() // dmg = 1 + pAttackDamageModifier; speed = 4 + pAttackSpeedModifier
                     .tab(ModItemGroups.VALCRAFT_TOOLS_ITEM_GROUP)));
+
+    public static  final RegistryObject<Item> BATTLEAXE = ITEMS.register("battleaxe",
+            () -> new AxeItem());
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
